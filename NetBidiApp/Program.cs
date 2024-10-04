ba77256f4727b29c51f089f24267824f0ddfcb6c
Roddy Rappaport <roddy.rappaport@gmail.com>
2024-10-04 17:10:52 +0300

    Improved NetBidiApp
    Using System.CommandLine instead of Mono.Options in order to provide a
    richer CLI interface which allows users to control the input/output
    files, unicode escaping, and text direction.

--- NetBidiApp/Program.cs
+++ NetBidiApp/Program.cs
@@ -1,82 +1,94 @@
-﻿using System;
-using NetBidi;
-using Mono.Options;
+﻿using System.CommandLine;
 using System.Text;
+using NetBidi;
 
 namespace NetBidiApp
 {
+    enum UnicodeEscape {
+        ALL,
+        UNICODE,
+        NONE
+    }
+
     internal class Program
     {
-        static readonly string USAGE_STRING = "[-h] FILENAME";
-
-        static string ReadAllStdin() {
-            using StreamReader sr = new(Console.OpenStandardInput(), Console.InputEncoding);
+        static string ReadFile(string filePath) {
+            using StreamReader sr = filePath == "-" ? new(Console.OpenStandardInput(), Console.InputEncoding) : new(filePath);
             return sr.ReadToEnd();
         }
 
-        static string EncodeNonAsciiCharacters( string value ) {
-            StringBuilder sb = new();
-            foreach(char c in value) {
-                // if( c > 127 ) {
-                    // This character is too big for ASCII
-                    sb.Append("\\u" + ((int) c).ToString("x4"));
-                // }
-                // else {
-                //     sb.Append(c);
-                // }
+        static void WriteFile(string filePath, string fileContents) {
+            using StreamWriter sw = filePath == "-" ? new(Console.OpenStandardOutput(), Console.OutputEncoding) : new(filePath);
+            sw.Write(fileContents);
+        }
+
+        static string EscapeText(string text, UnicodeEscape unicodeEscaping) {
+            if (unicodeEscaping == UnicodeEscape.NONE) {
+                return text;
             }
 
-            return sb.ToString();
+            StringBuilder escapedBuilder = new();
+            foreach(char currentChar in text) {
+                // If the character cannot be displayed in 7 bits (Which is sbyte max) then it's not
+                // ASCII.
+                if (unicodeEscaping == UnicodeEscape.ALL || currentChar > sbyte.MaxValue) {
+                    escapedBuilder.Append($"\\u{(int)currentChar:x4}");
+                }
+                else {
+                    escapedBuilder.Append(currentChar);
+                }
+            }
+
+            return escapedBuilder.ToString();
         }
 
-
-        static void PrintHelp() {
-            Console.WriteLine($"Usage: netbidi {USAGE_STRING}");
-        }
-
-        static TextDirection StringToTextDirection(string text) {
-            return text switch {
-                "right" or "r" => TextDirection.RTL,
-                "left" or "l" => TextDirection.LTR,
-                "neutral" or "n" => TextDirection.NEUTRAL,
-                _ => throw new Exception()
-            };
-        }
-
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            bool showHelp = false;
-            string filename = "-";
-            TextDirection textDirection = TextDirection.NEUTRAL;
+            Argument<string> inputFileArgument = new(
+                name: "input_file",
+                description: "The file to read and parse the value of. Use '-' for stdin.",
+                getDefaultValue: () => "-"
+            );
 
-            var optionSet = new OptionSet () {
-                { "f|filename=", "The file to change the order of",
-                    v => filename = v },
-                { "d|direction=", "The direction of the text",
-                    (string v) => textDirection = StringToTextDirection(v)},
-                { "h|?|help",  "Show the program's usage string", 
-                    v => showHelp = v != null },
-            };
+            Option<string> outputFileOption = new(
+                name: "--output",
+                description: "The file to write the converted string to. Use '-' for stdout.",
+                getDefaultValue: () => "-"
+            );
+            outputFileOption.AddAlias("-o");
 
-            // TODO: Detect invalid options and the like.
-            optionSet.Parse(args);
+            Option<TextDirection> directionOption = new(
+                name: "--direction",
+                description: "The paragraph default direction.",
+                getDefaultValue: () => TextDirection.NEUTRAL
+            );
+            directionOption.AddAlias("-d");
 
-            if (showHelp) {
-                PrintHelp();
-                return;
-            }
+            Option<UnicodeEscape> unicodeEscapingOption = new(
+                name: "--unicode-escape",
+                description: "Escape characters to the format of \\uXXXX.",
+                getDefaultValue: () => UnicodeEscape.NONE
+            );
+            unicodeEscapingOption.AddAlias("-u");
 
-            string inputText;
-            if (filename == "-") {
-                inputText = ReadAllStdin();
-            }
-            else {
-                inputText = File.ReadAllText(filename);
-            }
+            RootCommand rootCommand = new("Use NetBidi to reorder logical strings");
+            rootCommand.AddArgument(inputFileArgument);
+            rootCommand.AddOption(outputFileOption);
+            rootCommand.AddOption(directionOption);
+            rootCommand.AddOption(unicodeEscapingOption);
 
-            uint[] logicalString = NetBidi.NetBidi.ConvertString(inputText);
-            string displayString = NetBidi.NetBidi.BidiResolveString(logicalString, textDirection);
-            Console.WriteLine(EncodeNonAsciiCharacters(displayString));
+            rootCommand.SetHandler((inputFile, outputFile, direction, unicodeEscaping) => 
+                { 
+                    uint[] logicalString = NetBidi.NetBidi.ConvertString(ReadFile(inputFile));
+
+                    string visualString = NetBidi.NetBidi.BidiResolveString(logicalString, direction);
+                    visualString = EscapeText(visualString, unicodeEscaping);
+
+                    WriteFile(outputFile, visualString); 
+                },
+                inputFileArgument, outputFileOption, directionOption, unicodeEscapingOption);
+
+            return rootCommand.Invoke(args);
         }
     }
 }
